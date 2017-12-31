5d2397019ba7cab7e07ef8c33898b347133b8aca
Kirill Golowko <kirillgolowko@gmail.com>
2017-12-31 16:06:25 +0300

    Maven build-fix

---
+++ console-app/src/main/java/ru/kirillgolowko/formsToDocx/consoleApp/Main.java
@@ -0,0 +1,8 @@
+package ru.kirillgolowko.formsToDocx.consoleApp;
+import ru.kirillgolowko.formsToDocx.core.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, Forms");
+        }
+}
